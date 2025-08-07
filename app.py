5be5a94a00c1936f0094606ad5e3a0f9e2d72341
jacob <jacobhwei@outlook.com>
2025-08-07 11:48:38 -0700

    added openai model

--- app.py
+++ app.py
@@ -4,4 +4,8 @@ import chromadb
 from openai import OpenAI
 from chromadb.utils import embedding_functions
 
+load_dotenv()
+openapi_api_key = os.getenv("OPENAI_API_KEY")
 
+openai_ef = embedding_functions.OpenAIEmbeddingFunction(
+    api_key=openapi_api_key, model_name="gpt-3.5-turbo")
